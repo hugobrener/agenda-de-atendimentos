f5066e50928fe84dea3b96d9d8a2ed688d94c397
Hugo-Brener <hbrener82@gmail.com>
2023-02-22 10:46:46 -0300

    começando as interações

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let agendamento = new Object();
+agendamento.profissional = "Lucas";
+agendamento.servico = "corte";
+agendamento.data = "23-02-2023";
+agendamento.horario = "08:00";
+
+console.log(agendamento)
